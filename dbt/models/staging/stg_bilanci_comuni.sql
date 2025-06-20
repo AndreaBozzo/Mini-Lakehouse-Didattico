5b1af00fcb88a99ddc63bde1c8ff5c04ecd4f14e
poinT92 <andreabozzo92@gmail.com>
2025-06-20 12:14:09 +0200

    Aggiunto modello stg_bilanci_comuni.sql per dbt (livello staging)

---
+++ dbt/models/staging/stg_bilanci_comuni.sql
@@ -0,0 +1,10 @@
+{{ config(materialized='view') }}
+
+SELECT
+  codice_comune,
+  nome_comune,
+  anno,
+  entrate_tributarie,
+  spese_correnti,
+  entrate_tributarie - spese_correnti AS saldo
+FROM raw_bilanci
