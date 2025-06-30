448bf38fe5a582adc7f6958b69645f9d6421fa7f
poinT92 <andreabozzo92@gmail.com>
2025-06-30 17:49:34 +0200

    refactor: remove export of `export_marts` to simplify package API

--- audit/__init__.py
+++
@@ -1,5 +0,0 @@
-from .export_marts import export_marts
-
-__all__ = ["export_marts"]
-# Exporta la funzione `export_marts` per l'uso in altri moduli
-# Questo permette di mantenere l'API del pacchetto pulita e chiara.
